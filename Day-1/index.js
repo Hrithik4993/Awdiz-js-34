179e8425841e7470f208e7b095621b67519d94ea
Hrithik4993 <kokatehrithik@gmail.com>
2026-02-19 16:32:47 +0530

    updates

---
+++ Day-1/index.js
@@ -0,0 +1,26 @@
+var age = 20
+console.log(age,"age")
+age = 21
+console.log(age,"age new")
+var age= 24
+console.log(age,"age new 2")
+
+let number = 500
+console.log(number,"number")
+number =501
+console.log(number,"number2")
+
+const number2 =30
+console.log(number2,"number2")
+
+
+let a = 35
+{
+    var b = 40
+    {console.log(a)
+        const c = 45
+        console.log(c)
+    }
+   
+}
+console.log(b)
