09f1aa1985053daf1635303f32133e958b0e00af
Hrithik4993 <kokatehrithik@gmail.com>
2026-02-20 16:04:31 +0530

    updates

---
+++ Day-3/index.js
@@ -0,0 +1,19 @@
+var age = 18
+var maxagelimit = 80
+
+if(age>= 18 && age<= maxagelimit){
+    console.log("eligible to vote")
+}else{
+    console.log("not eligible")
+}
+
+var fruitname = "apple"
+ 
+switch (fruitname){
+    case "apple":
+    console.log("its apple")
+
+    case "watermelon":
+        console.log("its watermelon")
+
+}
--- Day-8/index.js
+++ Day-8/index.js
@@ -1,4 +1,4 @@
-push example
+// push example
 var number= [1,2,3,4,5];
 number.push(6);
 console.log(number);
