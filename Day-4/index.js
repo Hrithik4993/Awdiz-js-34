19ab090988a6a40d58d9c4a79ade41bfbdc0e8df
Hrithik4993 <kokatehrithik@gmail.com>
2026-02-20 17:40:50 +0530

    updates

---
+++ Day-4/index.js
@@ -0,0 +1,34 @@
+// // for even Numbers till 200
+for(i =0 ; i<= 200; i++) {
+   if(i % 2==0) {
+    console.log(i)
+   }
+}
+
+// // for gap of 2 till 200
+for(i=0; i <=200; i+=2 ) {
+    console.log(i)
+}
+
+// for gap of 10 from 100 to 200
+for(i=100; i<=200; i+= 10){
+    console.log(i)
+}
+// for numbers till 100
+for(i=0;i<=100;i++){
+    console.log(i)
+}
+
+// count of even and odd Number
+for(i =0 ; i<= 200; i++) {
+   if(i % 2==0) {
+    console.log(i, "even number ")
+   } else{
+console.log(i,"odd number")
+
+   }
+
+}
+
+
+
